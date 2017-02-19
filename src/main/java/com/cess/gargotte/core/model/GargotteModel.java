ad742ccceeb4590440ce394268c01adbd1a1e2b8
Notmoo <skraig.boeuf@gmail.com>
2017-02-19 23:14:31 +0100

    GargotteModel : ajout signatures méthds publiques

--- src/main/java/com/cess/gargotte/core/model/GargotteModel.java
+++ src/main/java/com/cess/gargotte/core/model/GargotteModel.java
@@ -1,5 +1,7 @@
 package com.cess.gargotte.core.model;
 
+import com.cess.gargotte.core.model.listeners.IModelFirerer;
+import com.cess.gargotte.core.model.listeners.ModelFirerer;
 import com.cess.gargotte.core.model.products.IProduct;
 import com.cess.gargotte.log.ILogger;
 import com.cess.gargotte.log.SimpleLogger;
@@ -8,7 +10,10 @@ import com.cess.gargotte.reader.SerIOHandler;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Guillaume on 19/02/2017.
@@ -19,12 +24,48 @@ public class GargotteModel {
 
     private IIOHandler ioHandler;
     private ILogger logger;
-    private List<IProduct> products;
+
+    private Map<String, List<IProduct>> products;
+    private IModelFirerer firerer;
 
 
     public GargotteModel(){
         ioHandler = new SerIOHandler();
         logger = new SimpleLogger();
-        products = ioHandler.read();
+        products = new HashMap<String, List<IProduct>>();
+
+        List<IProduct> products = ioHandler.read();
+        for(IProduct product : products){
+            if(!this.products.containsKey(product.getCat())){
+                this.products.put(product.getCat(), new ArrayList<IProduct>());
+            }
+            this.products.get(product.getCat()).add(product);
+        }
+
+        firerer = new ModelFirerer();
+    }
+
+    public List<IProduct> getProducts(String cat){
+        if(this.products.containsKey(cat)){
+            return this.products.get(cat);
+        }else{
+            return new ArrayList<IProduct>();
+        }
+    }
+
+    public List<String> getCatList(){
+        return new ArrayList<String>(this.products.keySet());
+    }
+
+    public boolean bufferSale(IProduct product, int quantity){
+        //TODO algo ventes
+    }
+
+    public boolean unbufferSale(IProduct, int quantity){
+        //TODO algo ventes
+    }
+
+        public void flushBufferedSales(){
+        //TODO algo ventes
     }
 }
