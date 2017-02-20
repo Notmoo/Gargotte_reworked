904277c54148677fd4466d370ff0b8d1c85e81ab
Notmoo <skraig.boeuf@gmail.com>
2017-02-20 22:56:13 +0100

    Rédaction du corps des méthodes

--- src/main/java/com/cess/gargotte/core/model/GargotteModel.java
+++ src/main/java/com/cess/gargotte/core/model/GargotteModel.java
@@ -3,6 +3,9 @@ package com.cess.gargotte.core.model;
 import com.cess.gargotte.core.model.listeners.IModelFirerer;
 import com.cess.gargotte.core.model.listeners.ModelFirerer;
 import com.cess.gargotte.core.model.products.IProduct;
+import com.cess.gargotte.core.model.sales.Order;
+import com.cess.gargotte.core.model.sales.ProductBuffer;
+import com.cess.gargotte.core.model.sales.Sale;
 import com.cess.gargotte.log.ILogger;
 import com.cess.gargotte.log.SimpleLogger;
 import com.cess.gargotte.reader.IIOHandler;
@@ -11,9 +14,9 @@ import com.cess.gargotte.reader.SerIOHandler;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * Created by Guillaume on 19/02/2017.
@@ -24,48 +27,69 @@ public class GargotteModel {
 
     private IIOHandler ioHandler;
     private ILogger logger;
-
-    private Map<String, List<IProduct>> products;
+    private ProductBuffer productBuffer;
+    private List<IProduct> products;
     private IModelFirerer firerer;
 
 
     public GargotteModel(){
         ioHandler = new SerIOHandler();
         logger = new SimpleLogger();
-        products = new HashMap<String, List<IProduct>>();
-
-        List<IProduct> products = ioHandler.read();
-        for(IProduct product : products){
-            if(!this.products.containsKey(product.getCat())){
-                this.products.put(product.getCat(), new ArrayList<IProduct>());
-            }
-            this.products.get(product.getCat()).add(product);
-        }
-
+        products =  ioHandler.read();
         firerer = new ModelFirerer();
+        productBuffer = new ProductBuffer();
     }
 
-    public List<IProduct> getProducts(String cat){
-        if(this.products.containsKey(cat)){
-            return this.products.get(cat);
-        }else{
-            return new ArrayList<IProduct>();
-        }
+    public List<IProduct> getProducts() {
+        return products;
+    }
+
+    public List<IProduct> getProducts(final String cat){
+        final List<IProduct> reply = new ArrayList<IProduct>();
+        this.products.stream().filter(new Predicate<IProduct>(){
+            public boolean test(IProduct product){
+                return product.getCat().equals(cat);
+            }
+        }).forEach(new Consumer<IProduct>() {
+            public void accept(IProduct product) {
+                reply.add(product);
+            }
+        });
+        return reply;
     }
 
     public List<String> getCatList(){
-        return new ArrayList<String>(this.products.keySet());
+        final List<String> reply = new ArrayList<String>();
+        for(IProduct product : this.products){
+            if(!reply.contains(product.getCat())){
+                reply.add(product.getCat());
+            }
+        }
+        return reply;
     }
 
-    public boolean bufferSale(IProduct product, int quantity){
-        //TODO algo ventes
+    public boolean bufferSale(IProduct product){
+        return productBuffer.addProduct(product);
     }
 
-    public boolean unbufferSale(IProduct, int quantity){
-        //TODO algo ventes
+    public boolean unbufferSale(IProduct product){
+       return productBuffer.removeProduct(product);
     }
 
-        public void flushBufferedSales(){
-        //TODO algo ventes
+    public boolean flushBufferedSales(){
+       Order order = this.productBuffer.flush();
+       this.productBuffer = new ProductBuffer();
+
+       this.logger.log(order);
+       //TODO Apply sur une copie, puis replacer la liste initiale (sécurité)
+       this.apply(order);
+       this.ioHandler.write(this.products);
+       return true;
+    }
+
+    private void apply(Order order){
+        for(Sale sale : order.getSales()){
+            this.products.get(this.products.indexOf(sale.getProduct())).applySale(sale.getAmount());
+        }
     }
 }
