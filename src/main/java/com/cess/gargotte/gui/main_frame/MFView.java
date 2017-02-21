93a702eb3c89b6401d02c2ae41e33f9bef9e7975
Notmoo <skraig.boeuf@gmail.com>
2017-02-21 22:09:50 +0100

    [GUI] Ajout MainFrame

---
+++ src/main/java/com/cess/gargotte/gui/main_frame/MFView.java
@@ -0,0 +1,34 @@
+package com.cess.gargotte.gui.main_frame;
+
+import javafx.scene.Parent;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+/**
+ * Created by Guillaume on 21/02/2017.
+ */
+public class MFView {
+    
+    private BorderPane mainPane;
+    private MFCtrl ctrl;
+    private TabPane tabPane;
+    
+    public MFView (MFCtrl ctrl) {
+        this.ctrl = ctrl;
+        this.mainPane = new BorderPane();
+        this.tabPane = new TabPane();
+        mainPane.setCenter(tabPane);
+    }
+    
+    public Parent getMainPane ( ) {
+        return mainPane;
+    }
+    
+    public void addModule(String name, Pane content){
+        Tab tab = new Tab(name);
+        tab.setContent(content);
+        this.tabPane.getTabs().add(tab);
+    }
+}
---
+++ src/main/java/com/cess/gargotte/gui/main_frame/MainFrame.java
@@ -0,0 +1,34 @@
+package com.cess.gargotte.gui.main_frame;
+
+import com.cess.gargotte.gui.modules.IModule;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Guillaume on 21/02/2017.
+ */
+public class MainFrame {
+    
+    private Stage stage;
+    private MFCtrl ctrl;
+    private MFView view;
+    
+    public MainFrame(Stage stage){
+        this.stage = stage;
+        this.ctrl = new MFCtrl();
+        this.view = new MFView(ctrl);
+        
+        this.stage.setScene(new Scene(view.getMainPane()));
+    }
+    
+    public void addModule(IModule module){
+        this.view.addModule(module.getName(), module.getPane());
+    }
+    
+    public void show(){
+        this.stage.show();
+    }
+}
