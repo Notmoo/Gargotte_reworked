4a070b1eb4853e946cc234c43c3573fac6d5e3fa
Notmoo <skraig.boeuf@gmail.com>
2017-02-21 20:29:15 +0100

    SimpleLogger : écriture contenu méthds

--- src/main/java/com/cess/gargotte/log/SimpleLogger.java
+++ src/main/java/com/cess/gargotte/log/SimpleLogger.java
@@ -1,14 +1,50 @@
 package com.cess.gargotte.log;
 
 import com.cess.gargotte.core.model.sales.Order;
+import com.cess.gargotte.core.model.sales.Sale;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Created by Guillaume on 19/02/2017.
  */
 public class SimpleLogger implements ILogger{
 
+    private final Path path;
+    private final ISaleLogSyntax syntax;
+
+    public SimpleLogger(Path path, SaleLogSyntaxFactory syntaxFactory){
+        this.path = path;
+        this.syntax = syntaxFactory.newInlineSimpleSyntax();
+    }
+
     public boolean log(Order order) {
-        //TODO Faire builder
-        return false;
+        boolean success = false;
+        try (FileWriter fw = new FileWriter(path.toString(), true);
+                 BufferedWriter bw = new BufferedWriter(fw);
+                 PrintWriter out = new PrintWriter(bw)) {
+                StringBuffer sb = new StringBuffer();
+
+                for(Sale sale : order.getSales()) {
+                    sb.append(syntax.applySyntax(sale)).append("\n");
+                }
+
+                out.append(sb.toString());
+                success = true;
+            }catch(FileNotFoundException e){
+                try{
+                    Files.createFile(path);
+                    success = log(order);
+                }catch(IOException ioe){
+                    ioe.printStackTrace();
+                    success = false;
+                }
+            }catch(IOException ioe) {
+                ioe.printStackTrace();
+                success = false;
+            }
+        return success;
     }
 }
