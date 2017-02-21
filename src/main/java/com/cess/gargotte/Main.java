cf18454f02e0eb9dd8522fd6211566cdc696c72e
Notmoo <skraig.boeuf@gmail.com>
2017-02-21 22:10:24 +0100

    [GUI] Main : modif en application JFX

--- src/main/java/com/cess/gargotte/Main.java
+++ src/main/java/com/cess/gargotte/Main.java
@@ -1,22 +1,27 @@
 package com.cess.gargotte;
 
 import com.cess.gargotte.core.model.GargotteModel;
-import com.cess.gargotte.core.model.products.ComposedProduct;
-import com.cess.gargotte.core.model.products.IProduct;
-import com.cess.gargotte.core.model.products.SimpleProduct;
-import com.cess.gargotte.reader.IIOHandler;
-import com.cess.gargotte.reader.SerIOHandler;
-
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import com.cess.gargotte.gui.main_frame.MainFrame;
+import com.cess.gargotte.gui.modules.order.OrderModule;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * Created by Guillaume on 21/02/2017.
  */
-public class Main {
+public class Main extends Application{
 
     public static final void main(String[] args){
+        launch(args);
+    }
+    
+    @Override
+    public void start (Stage primaryStage) throws Exception {
         GargotteModel model = new GargotteModel();
+        
+        MainFrame mf = new MainFrame(primaryStage);
+        mf.addModule(new OrderModule(model));
+        
+        mf.show();
     }
 }
